807c170d40e01d7a07f629e4bc97e7e07802f494
Roy <58099388+RoyMk@users.noreply.github.com>
2025-04-16 13:29:56 -0400

    first commit

---
+++ endpoints.py
@@ -0,0 +1,13 @@
+import requests
+
+class Endpoints:
+
+    all_apps = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+    player_count = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?"
+
+    def __init__(self):
+        pass
+
+
+
+
